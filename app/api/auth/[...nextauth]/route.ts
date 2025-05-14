949c034c4c5c883a7d758bbc21a30b652d7a6a40
kimyoungyin <yin199859@gmail.com>
2025-05-15 00:49:45 +0900

    fix: 앱 dev 환경에서fast refresh 후에도 단 하나의 prisma client만 존재하도록 수정

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,7 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 import GoogleProvider from "next-auth/providers/google";
-
-const prisma = new PrismaClient();
+import { prisma } from "@/lib/prisma";
 
 const authOptions = {
     adapter: PrismaAdapter(prisma),
