3fc3a7f75b6729953c0bc15dbf2014fc0db8b1d4
kimyoungyin <yin199859@gmail.com>
2025-05-23 19:32:15 +0900

    feat: next.config.ts에 썸네일 hostname, body 용량 30mb로 확장

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+                pathname: "/a/**",
+            },
+        ],
+    },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "30mb",
+        },
+    },
 };
 
 export default nextConfig;
